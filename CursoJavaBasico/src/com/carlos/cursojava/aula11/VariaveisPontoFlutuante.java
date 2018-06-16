c22e35228afb4cf745634834f8fb323b2c060235
crsilva3 <crsilva.mail@gmail.com>
2018-06-15 21:35:26 -0300

    Continuação das atividades

---
+++ CursoJavaBasico/src/com/carlos/cursojava/aula11/VariaveisPontoFlutuante.java
@@ -0,0 +1,15 @@
+package com.carlos.cursojava.aula11;
+
+public class VariaveisPontoFlutuante {
+
+	public static void main(String[] args) {
+		
+		double valorPassagem = 2.90;
+		float valorTomate = 3.90f;
+		System.out.println("Valor da passagem= "+ valorPassagem);
+		System.out.println("Valor do tomate= "+ valorTomate);
+				
+
+	}
+
+}
