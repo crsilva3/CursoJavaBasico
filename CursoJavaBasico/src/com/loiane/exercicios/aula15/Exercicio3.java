f60b34f3d1d524d1d3481c8761750ec84537cbd6
Carlos Ribeiro da Silva <crsilva.mail@gmail.com>
2019-11-03 23:24:36 -0300

    Continuação
    Adição de codigo

---
+++ CursoJavaBasico/src/com/loiane/exercicios/aula15/Exercicio3.java
@@ -0,0 +1,14 @@
+package com.loiane.exercicios.aula15;
+
+public class Exercicio3 {
+
+	public static void main(String[] args) {
+		
+		
+		/* 3. Faça um Programa que verifique se uma letra digitada é "F" ou "M".
+		Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.*/
+		
+
+	}
+
+}
